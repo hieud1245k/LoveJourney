9f9b69ac9ea1594e2fe7e6541ed3c5a77611e922
prd-hieu-nguyen-minh <hieu.nguyen.minh@paradox.ai>
2022-03-30 11:37:54 +0700

    Update code

--- app/src/main/java/com/hieuminh/lovejourney/MainActivity.kt
+++ app/src/main/java/com/hieuminh/lovejourney/MainActivity.kt
@@ -3,7 +3,7 @@ package com.hieuminh.lovejourney
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.hieuminh.lovejourney.adapter.HomeAdapter
+import com.hieuminh.lovejourney.adapter.EventAdapter
 import com.hieuminh.lovejourney.common.constants.AppPatterns
 import com.hieuminh.lovejourney.databinding.ActivityMainBinding
 import com.hieuminh.lovejourney.models.Couple
@@ -14,7 +14,8 @@ import com.hieuminh.lovejourney.utils.StringUtil
 import com.hieuminh.lovejourney.view.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
-    private lateinit var mAdapter: HomeAdapter
+    private lateinit var eventAdapter: EventAdapter
+    private var couple: Couple? = null
 
     override fun getViewBinding() = ActivityMainBinding.inflate(this.layoutInflater)
 
@@ -26,18 +27,24 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun initListener() = Unit
 
     override fun initView() {
-        val json =
-            applicationContext.assets.open("mock_data.json").bufferedReader().use { it.readText() }
-        val couple = JsonUtil.fromJson<Couple>(json)
+        val json = assets.open("mock_data.json").bufferedReader().use { it.readText() }
+        couple = JsonUtil.fromJson<Couple>(json)
         binding.tvMaleName.text = couple?.maleName
         binding.tvFemaleName.text = couple?.femaleName
         val days = DateUtil.getDateCount(couple?.confessionDay ?: "", AppPatterns.DATE_PATTERN)
         binding.tvDayNumber.text =
             StringUtil.getString(this, R.string.s_day)?.let { String.format(it, days) }
-        mAdapter = HomeAdapter()
-        mAdapter.updateData(couple?.events as MutableList<Event>?)
-        binding.rvEvent.layoutManager = LinearLayoutManager(null)
-        binding.rvEvent.setHasFixedSize(true)
-        binding.rvEvent.adapter = mAdapter
+
+        eventAdapter = EventAdapter()
+        eventAdapter.updateData(couple?.events as MutableList<Event>?, true)
+        binding.rvEvent.run {
+            layoutManager = LinearLayoutManager(null)
+            adapter = eventAdapter
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 }
---
+++ app/src/main/java/com/hieuminh/lovejourney/adapter/EventAdapter.kt
@@ -0,0 +1,31 @@
+package com.hieuminh.lovejourney.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.hieuminh.lovejourney.adapter.base.BaseAdapter
+import com.hieuminh.lovejourney.databinding.ItemEventBinding
+import com.hieuminh.lovejourney.models.Data
+import com.hieuminh.lovejourney.models.Event
+
+class EventAdapter : BaseAdapter<Event>() {
+    private var dataAdapters: List<DataAdapter>? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
+        val binding = ItemEventBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return EventViewHolder(binding)
+    }
+
+    inner class EventViewHolder(private val binding: ItemEventBinding) :
+        BaseViewHolder(binding.root) {
+        override fun bind(data: Event) {
+            binding.ivDate.text = data.date
+            val dataAdapter = dataAdapters?.get(layoutPosition)
+            dataAdapter?.updateData(data.dataList as MutableList<Data>?)
+        }
+    }
+
+    override fun updateData(list: MutableList<Event>?, isReload: Boolean) {
+        list?.let { dataAdapters = List(list.size) { DataAdapter() } }
+        super.updateData(list, isReload)
+    }
+}
